91675a270503820c8af5d76a4b1edb47e7382633
sukhepadda <paddasukhwantsingh@gmail.com>
2023-02-27 19:07:35 +0530

    continuing the code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Header from "./components/Header"
 function App() {
   return (
     <Router>
+    <Header />
       <Routes>
         <Route />
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+
+function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
+        pos={'fixed'}
+        top={'4'}
+        left={'4'}
+        colorScheme={'purple'}
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+        // onClick={onClose}
+      >
+        <BiMenuAltLeft />
+      </Button>
+
+      <Drawer isOpen={isOpen} placement={'left'} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Padda Video Hub</DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button variant={'ghost'} colorScheme={'purple'}>
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme={'purple'}>
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme={'purple'}>
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+              <Button variant={'ghost'} colorScheme={'purple'}>
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
+            </VStack>
+            <HStack pos={'absolute'} bottom={'10'} left={'0'} w={'full'} justifyContent={"space-evenly"}>
+              <Button colorScheme={'purple'}>
+                <Link to={'/login'}>Login</Link>
+              </Button>
+              <Button colorScheme={'purple'}>
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+
+export default Header;
